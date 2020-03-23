ba9ca6a8beef29db2ea3889e92a615233eb1041d
stefpi <stef@ceatanostra.org>
2020-03-23 01:04:49 -0400

    version 2

--- app.js
+++ app.js
@@ -8,49 +8,56 @@ const pug = require('pug');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 8080;
 
 app.set('view engine', 'pug');
 app.set("views", p.join(__dirname, "views"));
-app.use(express.static('public'));
+app.locals.basedir = p.join(__dirname, "views");
+// app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.json());
 let upload = multer({dest: 'uploads/'});
 
 let clips = [];
 let wordID = [];
-var id;
 
 app.get('/', function(req, res) {
     res.render('index');
 });
 
 app.post('/', upload.single('userFile'), function(req, response) {
-    // res.render('index');
-    // res.writeHead(200, {'Content-Type': 'text/html'});
+    //647iaau8s2ovn7ywasvuwxpjbhttrj
+    let clientId = req.body.userId;
+    console.log(clientId);
 
     let userFilePath = req.file.path;
 
     let file = fs.readFileSync(userFilePath, 'utf8');
     clips = file.toString().split('\n');
-    id = clips[0].split(' ')[0];
-    clips.splice(0, 1);
-    console.log(clips);
-    console.log(id);
+    // id = clips[0].split(' ')[0];
+    // clips.splice(0, 1);
+    // console.log(clips);
+    // console.log(id);
     for (let i = 0; i < clips.length; i++) {
         console.log(clips[i]);
         wordID[i] = clips[i].split(".tv/").pop().split(" ")[0];
         console.log(wordID[i] + "\n");
     }
 
+    console.log(userFilePath);
+
     fs.unlinkSync(userFilePath);
     let videoLinks = [];
     let videoTitles = [];
+    let thumbImg = [];
     let index = 0;
 
     for (let i = 0; i < clips.length; i++) {
         const options = {
             url: 'https://api.twitch.tv/helix/clips?id=' + wordID[i],
             headers: {
-                'Client-ID': id
+                'Client-ID': clientId
             }
         };
 
@@ -67,29 +74,13 @@ app.post('/', upload.single('userFile'), function(req, response) {
             // videoTitles.push(vidTitle);
             videoTitles[i] = contents.data[0].title;
 
-            if(index == clips.length - 1) {
-                response.render('links', {links: videoLinks, titles: videoTitles})
+            thumbImg[i] = thumbStr;
+
+            if(index === clips.length - 1) {
+                response.render('links', {links: videoLinks, titles: videoTitles, images: thumbImg});
             }
             index++;
         });
-
-        // rp(options)
-        //     .then(function($) {
-        //         const contents = JSON.parse($);
-        //         let thumbStr = contents.data[0].thumbnail_url;
-        //         let thumbArr = thumbStr.split("-preview-");
-        //         // let vidLink = thumbArr[0] + ".mp4";
-        //         // videoLinks.push(vidLink);
-        //         videoLinks[i] = thumbArr[0] + ".mp4";
-        //
-        //         // let vidTitle = contents.data[0].title;
-        //         // videoTitles.push(vidTitle);
-        //         videoTitles[i] = contents.data[0].title;
-        //     })
-        //     .catch(function(err) {
-        //         console.log(err);
-        //     });
-
     }
 });
 
--- linksDownload.js
+++
@@ -1,6 +0,0 @@
-$('a.filelinks').click(function(e) {
-    e.preventDefault();
-    for(let x=0; x < filelinks.length; c++) {
-        window.open(a.filelinks.x);
-    }
-});
